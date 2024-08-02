2c9a9bd11ec941b95bfc08557ce99d9a696f3ee5
Franklin Alves <franklin_kaio@hotmail.com>
2024-08-02 20:43:37 -0300

    Add e configurando o ImageField.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class Contactadmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', # listando os dados com as colunas.
+    ordering = 'id', # Ordena crescente 
+    #ordering = '-id', # Ordena decrescente
+    #list_filter = 'create_data', # Filtro por data
+    search_fields = 'id', 'first_name', 'last_name', # campo de pesquisa
+    list_per_page = 5 # paginação
+    list_max_show_all = 100 
+    #list_editable = 'first_name', 'last_name', 'phone', # Editar valores nas listas
+    list_display_links = 'id', 'first_name',
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('contact.urls')), # Inclui as URLs da aplicação 'contact'
     path('admin/', admin.site.urls),  # Adiciona a URL para o painel administrativo do Django
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
